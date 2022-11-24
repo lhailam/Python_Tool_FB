a85ae01737aa538a674160d2d3c322294d7a0431
LHLam <lehailam222@gmail.com>
2022-11-24 21:28:25 +0700

    update

---
+++ test.py
@@ -0,0 +1,14 @@
+# import random
+
+# chuoi = 'qwertyuiopasdfghjkllzxcvbnm '
+# for i in range(5):
+#     # kytu = random.randint(4,7)
+#     # a = ''.join(random.choice(chuoi) for _ in range(kytu))
+#     a = (''.join(random.choice('qwertyuiopasdfghjkllzxcvbnm ') for _ in range(random.randint(4,7))))
+#     print(a)
+# print(type(a))
+
+a= "5"
+b= int(a)+6
+print(type(a))
+print((b))
